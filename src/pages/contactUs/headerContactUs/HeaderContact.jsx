114b2beaa989570be0f32c9fb8613fac37e9a116
shady_adel <shadysalma090@gmail.com>
2024-02-28 23:50:57 +0200

    edit contactUs

---
+++ src/pages/contactUs/headerContactUs/HeaderContact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './headerContact.css'
+function HeaderContact() {
+  return (
+   <>
+   <section className="master">
+        <h1>Contact Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          expedita fugiat animi veniam rem minus eveniet voluptatibus, cum
+          aliquam. Repudiandae provident ab, commodi sit necessitatibus eius
+          accusamus cupiditate assumenda. Placeat.
+        </p>
+      </section>
+   
+   
+   </>
+  )
+}
+
+export default HeaderContact
